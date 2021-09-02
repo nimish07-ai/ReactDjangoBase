0dcd569cd237498e5016464a59e1c51c970c886c
nimish07-ai <nimishboda7@gmail.com>
2021-09-03 01:33:18 +0530

    init

---
+++ templates/react/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const webpack = require("webpack");
+
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./static/frontend"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localsConvention: 'camelCase',
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ],
+  },
+  optimization: {
+    minimize: true,
+  },
+  devServer: {
+    host: 'localhost',
+    port: port,
+    historyApiFallback: true,
+    open: true
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        // This has effect on the react lib size
+        NODE_ENV: JSON.stringify("production"),
+      },
+    }),
+  ],
+};
